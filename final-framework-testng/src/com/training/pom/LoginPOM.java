442c530f1a37e5df74ae9d79a7ed43c5ca5a240a
SwarnaliDas <rimidas1300@gmail.com>
2020-04-07 16:00:24 +0530

    Initial Check in by Swarnali Das

--- final-framework-testng/src/com/training/pom/LoginPOM.java
+++ final-framework-testng/src/com/training/pom/LoginPOM.java
@@ -1,38 +1,52 @@
 package com.training.pom;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import com.trianing.waits.WaitTypes;
 
 public class LoginPOM {
 	private WebDriver driver; 
+	private WaitTypes WaitTypes;
+	
 	
 	public LoginPOM(WebDriver driver) {
 		this.driver = driver; 
 		PageFactory.initElements(driver, this);
+		WaitTypes = new WaitTypes(driver);
 	}
 	
-	@FindBy(id="login")
+	//username locator
+	@FindBy(id="input-username")
 	private WebElement userName; 
 	
-	@FindBy(id="password")
+	//Password locator
+	@FindBy(id="input-password")
 	private WebElement password;
 	
-	@FindBy(id="formLogin_submitAuth")
+	//Login button locator
+	@FindBy(xpath="//*[contains(text(),'Login')]")
 	private WebElement loginBtn; 
-	
+
+	//Username entry function
 	public void sendUserName(String userName) {
 		this.userName.clear();
 		this.userName.sendKeys(userName);
 	}
 	
+	//Password Entry function
 	public void sendPassword(String password) {
 		this.password.clear(); 
 		this.password.sendKeys(password); 
 	}
 	
+	//Click Login button function
 	public void clickLoginBtn() {
 		this.loginBtn.click(); 
+		WaitTypes.waitForElement((By.id("button-menu")),30);
+		
 	}
 }
+
--- final-framework-testng/tests/com/training/sanity/tests/LoginTests.java
+++ final-framework-testng/tests/com/training/sanity/tests/LoginTests.java
@@ -10,8 +10,11 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import com.sun.jna.platform.win32.WTypes;
 import com.training.generics.ScreenShot;
 import com.training.pom.LoginPOM;
+import com.training.pom.SalesreportPOM;
+import com.training.pom.dashboardPOM;
 import com.training.utility.DriverFactory;
 import com.training.utility.DriverNames;
 
@@ -20,6 +23,8 @@ public class LoginTests {
 	private WebDriver driver;
 	private String baseUrl;
 	private LoginPOM loginPOM;
+	private dashboardPOM dashboardPOM;
+	private SalesreportPOM SalesreportPOM;
 	private static Properties properties;
 	private ScreenShot screenShot;
 
@@ -34,6 +39,8 @@ public class LoginTests {
 	public void setUp() throws Exception {
 		driver = DriverFactory.getDriver(DriverNames.CHROME);
 		loginPOM = new LoginPOM(driver); 
+		dashboardPOM = new dashboardPOM(driver);
+		SalesreportPOM = new SalesreportPOM(driver);
 		baseUrl = properties.getProperty("baseURL");
 		screenShot = new ScreenShot(driver); 
 		// open the browser 
@@ -45,11 +52,87 @@ public class LoginTests {
 		Thread.sleep(1000);
 		driver.quit();
 	}
-	@Test
-	public void validLoginTest() {
+	@Test (priority=1)	
+	public void validLoginTest() throws InterruptedException 
+	{
+		
+		//----------TEST-CASE-01-----------//
+		
+		//1. Giving Username
 		loginPOM.sendUserName("admin");
+		//2.Giving Password
 		loginPOM.sendPassword("admin@123");
+		//3. Clicking on Login Button
 		loginPOM.clickLoginBtn(); 
-		screenShot.captureScreenShot("First");
+		//4. Clicking on Expand Button
+		dashboardPOM.clickexpandbutton();
+		//5. Clicking on reports tab
+		dashboardPOM.Reports();
+		//6. Clicking on Sales tab
+		dashboardPOM.Sales();
+		//7. Clicking on Orders tab
+		dashboardPOM.Orders();
+		//8. Selecting from GroupBy Dropdown
+		SalesreportPOM.GroupBy();
+		//9. Clicking on Filter button to display the result
+		SalesreportPOM.Filter();
+		Thread.sleep(5000);
+		driver.close();
+	}
+	
+	@Test (priority=2)	
+	public void validLoginTest2() throws InterruptedException 
+	{
+		
+		//----------TEST-CASE-02-----------//
+		
+		//1. Giving Username
+		loginPOM.sendUserName("admin");
+		//2.Giving Password
+		loginPOM.sendPassword("admin@123");
+		//3. Clicking on Login Button
+		loginPOM.clickLoginBtn(); 
+		//4. Clicking on Expand Button
+		dashboardPOM.clickexpandbutton();
+		//5. Clicking on reports tab
+		dashboardPOM.Reports();
+		//6. Clicking on Sales tab
+		dashboardPOM.Sales();
+		//7. Clicking on Orders tab
+		dashboardPOM.Tax();
+		//8. Selecting from GroupBy Dropdown
+		SalesreportPOM.GroupBy();
+		//9. Clicking on Filter button to display the result
+		SalesreportPOM.Filter();
+		Thread.sleep(5000);
+		driver.close();
+	}
+	
+	@Test (priority=3)	
+	public void validLoginTest3() throws InterruptedException 
+	{
+		
+		//----------TEST-CASE-03-----------//
+		
+		//1. Giving Username
+		loginPOM.sendUserName("admin");
+		//2.Giving Password
+		loginPOM.sendPassword("admin@123");
+		//3. Clicking on Login Button
+		loginPOM.clickLoginBtn(); 
+		//4. Clicking on Expand Button
+		dashboardPOM.clickexpandbutton();
+		//5. Clicking on reports tab
+		dashboardPOM.Reports();
+		//6. Clicking on Sales tab
+		dashboardPOM.Sales();
+		//7. Clicking on Orders tab
+		dashboardPOM.Shipping();
+		//8. Selecting from GroupBy Dropdown
+		SalesreportPOM.GroupBy();
+		//9. Clicking on Filter button to display the result
+		SalesreportPOM.Filter();
+		Thread.sleep(5000);
+		driver.close();
 	}
 }
